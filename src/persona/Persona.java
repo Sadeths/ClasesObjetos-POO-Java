86f2a4151dc60e2402babde33efdff6258bb9cf7
Samahel Thomas <dereksamahel.thomas@gmail.com>
2026-04-09 02:01:59 -0600

    Initial commit

---
+++ src/persona/Persona.java
@@ -0,0 +1,34 @@
+package persona;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    //Constructor
+    public Persona (String nombre, String apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString(){
+        return "Nombre: " + this.nombre + ", Apellido: " + this.apellido + " , Dir. Mem. " + super.toString();
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getApellido(){
+        return this.apellido;
+    }
+
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+
+    
+}
